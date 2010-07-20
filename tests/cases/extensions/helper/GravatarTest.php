49500f427e738c5e050effb40ef9bacaa2290ef8
michaelhue <mail@froschlaich.com>
2010-07-20 23:50:32 +0200

    Changed test assertions from 'assertEqual' to 'assertTags'.

--- tests/cases/extensions/helper/GravatarTest.php
+++ tests/cases/extensions/helper/GravatarTest.php
@@ -21,30 +21,43 @@ class GravatarTest extends \lithium\test\Unit {
 	}
 	
 	public function testImageWithoutOptions() {
-		$expected = '<img src="http://gravatar.com/avatar/5b9c2b225b5c4ff91ffe849209153ecc?d=404&s=80&r=g" alt="" />';
 		$result = $this->helper->image('mail@example.org');
-		$this->assertEqual($expected, $result);
+		$expected = array(
+			'img' => array('src' => 'http://gravatar.com/avatar/5b9c2b225b5c4ff91ffe849209153ecc?d=404&s=80&r=g', 'alt' => '')
+		);
+		$this->assertTags($result, $expected);
 		
 		$result = $this->helper->image('MAIL@EXAMPLE.ORG');
-		$this->assertEqual($expected, $result);
+		$this->assertTags($result, $expected);
 	}
 	
 	public function testImageWithOptions() {
-		$expected = '<img src="http://gravatar.com/avatar/5b9c2b225b5c4ff91ffe849209153ecc?d=404&s=40&r=g" alt="" />';
 		$result = $this->helper->image('mail@example.org', array('size' => 40));
-		$this->assertEqual($expected, $result);
+		$expected = array(
+			'img' => array('src' => 'http://gravatar.com/avatar/5b9c2b225b5c4ff91ffe849209153ecc?d=404&s=40&r=g', 'alt' => '')
+		);
+		$this->assertTags($result, $expected);
 		
-		$expected = '<img src="http://gravatar.com/avatar/5b9c2b225b5c4ff91ffe849209153ecc?d=identicon&s=80&r=g" alt="" />';
 		$result = $this->helper->image('mail@example.org', array('default' => 'identicon'));
-		$this->assertEqual($expected, $result);
+		$expected = array(
+			'img' => array('src' => 'http://gravatar.com/avatar/5b9c2b225b5c4ff91ffe849209153ecc?d=identicon&s=80&r=g', 'alt' => '')
+		);
+		$this->assertTags($result, $expected);
 		
-		$expected = '<img src="http://gravatar.com/avatar/5b9c2b225b5c4ff91ffe849209153ecc?d=http%3A%2F%2Fexample.org%2Fdefault.jpg&s=80&r=g" alt="" />';
 		$result = $this->helper->image('mail@example.org', array('default' => 'http://example.org/default.jpg'));
-		$this->assertEqual($expected, $result);
+		$expected = array(
+			'img' => array('src' => 'http://gravatar.com/avatar/5b9c2b225b5c4ff91ffe849209153ecc?d=http%3A%2F%2Fexample.org%2Fdefault.jpg&s=80&r=g', 'alt' => '')
+		);
+		$this->assertTags($result, $expected);
 		
-		$expected = '<img src="http://gravatar.com/avatar/5b9c2b225b5c4ff91ffe849209153ecc?d=404&s=80&r=g" alt="Gravatar" class="gravatar" />';
 		$result = $this->helper->image('mail@example.org', array('alt' => 'Gravatar', 'class' => 'gravatar'));
-		$this->assertEqual($expected, $result);
+		$expected = array(
+			'img' => array(
+				'src' => 'http://gravatar.com/avatar/5b9c2b225b5c4ff91ffe849209153ecc?d=404&s=80&r=g', 
+				'alt' => 'Gravatar', 'class' => 'gravatar'
+			)
+		);
+		$this->assertTags($result, $expected);
 	}
 	
 }
