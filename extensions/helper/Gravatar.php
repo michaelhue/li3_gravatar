24ccaa3d70275b5cbcb9d61664d2ee47e13e589e
michaelhue <mail@froschlaich.com>
2011-03-13 13:32:58 +0100

    Added `Gravatar::url()` method in Helper. Updated tests.

--- extensions/helper/Gravatar.php
+++ extensions/helper/Gravatar.php
@@ -21,49 +21,57 @@ class Gravatar extends \lithium\template\Helper {
 	protected $_strings = array(
 		'gravatar_image' => '<img src="{:url}"{:options} />'
 	);
-
+	
 	/**
-	 * Renders the Gravatar for the given `$email` address.
+	 * Generates an URL to the Gravatar for `'$email'`.
 	 *
-	 * @param string $email  
-	 * @param array [$options] Optional options. May be any HTML attribute for the image tag and 
-	 * 				the following options:
-	 *              - default: The default image that will be served if there is no Gravatar
-	 *				           associated with `$email`. This can be a full URL to a default 
-	 *				           image, an absolute path, `mm`, `identicon`, `monsterid`, `wavatar` 
-	 * 				           or `404` (default).
-	 *              - size: Size in pixels of the Gravatar. Defaults to `80`.
-	 *              - rating: The required Gravatar rating. This can be `g`, `pg`, `r` or `x`.
-	 *				          If the Gravatar does not meet the minimum rating the default
-	 * 				          image will be returned. Defaults to `g`.
-	 * @return string Returns an image tag displaying the Gravatar.
+	 * @link http://gravatar.com/site/implement/images/ Description of the Gravatar options
+	 * @param string $email
+	 * @param array $options Optional options. Valid options are:
+	 *     - `'default'`: The fallback image that will be provided if there is no Gravatar associated 
+	 *     with `'$email'`. This may be a full URL, an absolute path, `'mm'`, `'identicon'`, 
+	 *     `'monsterid'`, `'wavatar'` or `'404'`. Defaults to `'404'`.
+	 *     - `'size'`: Size of the image in pixels. Defaults to `'80'`.
+	 *     - `'rating'`: Required minimum rating of the Gravatar. This may be `'g'`, `'pg'`, `'r'` or
+	 *     `'x'`. If the Gravatar does not meet the rating the `'default'` option will be returned.
+	 *     Defaults to `'g'`.
+	 * @return string Returns an URL to the Gravatar image.
 	 */
-	public function image($email, array $options = array()) {
-		$defaults = array(
-			'default' => 404,
-			'size' => 80,
-			'rating' => 'g',
-			'alt' => ''
-		);
+	public function url($email, array $options = array()) {
+		$defaults = array('default' => 404, 'size' => 80, 'rating' => 'g');
 		$options += $defaults;
-		$request = $this->_context->request();
 		
+		$request = $this->_context->request();
 		$defaultValues = array(404, 'mm', 'identicon', 'monsterid', 'wavatar');
+		$hash = md5(strtolower(trim($email)));
+	
 		if (!in_array($options['default'], $defaultValues) && strpos($options['default'], '://') === false) {
-			$host = $request->env('HTTP_HOST');
 			$scheme = $request->env('HTTPS') ? 'https://' : 'http://';
+			$host = $request->env('HTTP_HOST');
 			$options['default'] = $scheme . $host . $options['default'];
 		}
-		
-		$email = strtolower(trim($email));
-		$hash  = md5($email);
 		
 		$url  = "http://gravatar.com/avatar/{$hash}";
 		$url .= "?d=" . urlencode($options['default']);
 		$url .= "&s={$options['size']}";
 		$url .= "&r={$options['rating']}";
-		unset($options['default'], $options['size'], $options['rating']);
+		
+		return $url;
+	}
 
+	/**
+	 * Renders the Gravatar for `'$email'`.
+	 *
+	 * @see li3_gravatar\extensions\helpers\Gravatar::url()
+	 * @param string $email  
+	 * @param array $options Optional options.
+	 * @return string Returns an image tag displaying the Gravatar.
+	 */
+	public function image($email, array $options = array()) {
+		$defaults = array('default' => 404, 'size' => 80, 'rating' => 'g', 'alt' => '');
+		$options += $defaults;
+		$url = $this->url($email, $options);
+		unset($options['default'], $options['size'], $options['rating']);
 		return $this->_render(__METHOD__, 'gravatar_image', compact('url', 'options'));
 	}
 
--- tests/cases/extensions/helper/GravatarTest.php
+++ tests/cases/extensions/helper/GravatarTest.php
@@ -8,13 +8,11 @@
 
 namespace li3_gravatar\tests\cases\extensions\helper;
 
-use \li3_gravatar\extensions\helper\Gravatar;
-use \lithium\tests\mocks\action\MockControllerRequest;
-use \lithium\tests\mocks\template\helper\MockHtmlRenderer;
+use li3_gravatar\extensions\helper\Gravatar;
+use lithium\tests\mocks\action\MockControllerRequest;
+use lithium\tests\mocks\template\helper\MockHtmlRenderer;
 
 class GravatarTest extends \lithium\test\Unit {
-	
-	public $helper = null;
 	
 	public function setUp() {
 		$this->request = new MockControllerRequest();
@@ -22,47 +20,77 @@ class GravatarTest extends \lithium\test\Unit {
 		$this->helper = new Gravatar(array('context' => &$this->context));
 	}
 	
-	public function testImageWithoutOptions() {
+	public function testUrlWithoutOptions() {
+		$expected = 'http://gravatar.com/avatar/08aff750c4586c34375a0ebd987c1a7e?d=404&s=80&r=g';
+		$result = $this->helper->url('john@example.org');
+		$this->assertEqual($expected, $result);
+		
+		$expected = 'http://gravatar.com/avatar/978a05c7e2e60f547991b0ee47876553?d=404&s=80&r=g';
+		$result = $this->helper->url('george@example.org');
+		$this->assertEqual($expected, $result);
+		
+		$result = $this->helper->url('GEORGE@EXAMPLE.ORG');
+		$this->assertEqual($expected, $result);
+		
+		$result = $this->helper->url(' george@example.org ');
+		$this->assertEqual($expected, $result);
+	}
+	
+	public function testUrlWithSize() {
+		$expected = 'http://gravatar.com/avatar/08aff750c4586c34375a0ebd987c1a7e?d=404&s=40&r=g';
+		$result = $this->helper->url('john@example.org', array('size' => 40));
+		$this->assertEqual($expected, $result);
+	}
+	
+	public function testUrlWithDefault() {
+		$expected = 'http://gravatar.com/avatar/08aff750c4586c34375a0ebd987c1a7e?d=mm&s=80&r=g';
+		$result = $this->helper->url('john@example.org', array('default' => 'mm'));
+		$this->assertEqual($expected, $result);
+		
+		$expected = 'http://gravatar.com/avatar/08aff750c4586c34375a0ebd987c1a7e?d=identicon&s=80&r=g';
+		$result = $this->helper->url('john@example.org', array('default' => 'identicon'));
+		$this->assertEqual($expected, $result);
+		
+		$expected = 'http://gravatar.com/avatar/08aff750c4586c34375a0ebd987c1a7e?d=monsterid&s=80&r=g';
+		$result = $this->helper->url('john@example.org', array('default' => 'monsterid'));
+		$this->assertEqual($expected, $result);
+		
+		$expected = 'http://gravatar.com/avatar/08aff750c4586c34375a0ebd987c1a7e?d=wavatar&s=80&r=g';
+		$result = $this->helper->url('john@example.org', array('default' => 'wavatar'));
+		$this->assertEqual($expected, $result);
+		
+		$expected = 'http://gravatar.com/avatar/08aff750c4586c34375a0ebd987c1a7e?d=http%3A%2F%2Fexample.org%2Favatar.png&s=80&r=g';
+		$result = $this->helper->url('john@example.org', array('default' => 'http://example.org/avatar.png'));
+		$this->assertEqual($expected, $result);
+		
+		$host = $this->request->env('HTTP_HOST');
+		$scheme = $this->request->env('HTTPS') ? 'https://' : 'http://';
+		$default = urlencode("{$scheme}{$host}/img/avatar.png");
+		
+		$expected = "http://gravatar.com/avatar/08aff750c4586c34375a0ebd987c1a7e?d={$default}&s=80&r=g";
+		$result = $this->helper->url('john@example.org', array('default' => '/img/avatar.png'));
+		$this->assertEqual($expected, $result);
+	}
+	
+	public function testUrlWithRating() {
+		$expected = 'http://gravatar.com/avatar/08aff750c4586c34375a0ebd987c1a7e?d=404&s=80&r=x';
+		$result = $this->helper->url('john@example.org', array('rating' => 'x'));
+		$this->assertEqual($expected, $result);
+		
+		$expected = 'http://gravatar.com/avatar/08aff750c4586c34375a0ebd987c1a7e?d=404&s=80&r=pg';
+		$result = $this->helper->url('john@example.org', array('rating' => 'pg'));
+		$this->assertEqual($expected, $result);
+	}
+	
+	public function testImageWithoutAttributes() {
 		$result = $this->helper->image('mail@example.org');
 		$expected = array('img' => array(
 			'src' => 'http://gravatar.com/avatar/5b9c2b225b5c4ff91ffe849209153ecc?d=404&s=80&r=g', 'alt' => ''
 		));
 		$this->assertTags($result, $expected);
-		
-		$result = $this->helper->image('MAIL@EXAMPLE.ORG');
-		$this->assertTags($result, $expected);
 	}
 	
-	public function testImageWithOptions() {
-		$result = $this->helper->image('mail@example.org', array('size' => 40));
-		$expected = array('img' => array(
-			'src' => 'http://gravatar.com/avatar/5b9c2b225b5c4ff91ffe849209153ecc?d=404&s=40&r=g', 'alt' => ''
-		));
-		$this->assertTags($result, $expected);
-		
-		$result = $this->helper->image('mail@example.org', array('default' => 'identicon'));
-		$expected = array('img' => array(
-			'src' => 'http://gravatar.com/avatar/5b9c2b225b5c4ff91ffe849209153ecc?d=identicon&s=80&r=g', 'alt' => ''
-		));
-		$this->assertTags($result, $expected);
-		
-		$result = $this->helper->image('mail@example.org', array('default' => 'http://example.org/default.jpg'));
-		$expected = array('img' => array(
-			'src' => 'http://gravatar.com/avatar/5b9c2b225b5c4ff91ffe849209153ecc?d=http%3A%2F%2Fexample.org%2Fdefault.jpg&s=80&r=g', 
-			'alt' => ''
-		));
-		$this->assertTags($result, $expected);
-		
-		$host = $this->request->env('HTTP_HOST');
-		$scheme = $this->request->env('HTTPS') ? 'https://' : 'http://';
-		$default = urlencode("{$scheme}{$host}/default.jpg");
-		
-		$result = $this->helper->image('mail@example.org', array('default' => '/default.jpg'));
-		$expected = array('img' => array(
-			'src' => "http://gravatar.com/avatar/5b9c2b225b5c4ff91ffe849209153ecc?d={$default}&s=80&r=g", 'alt' => ''
-		));
-		$this->assertTags($result, $expected);
-		
+	public function testImageWithAttributes() {
 		$result = $this->helper->image('mail@example.org', array('alt' => 'Gravatar', 'class' => 'gravatar'));
 		$expected = array('img' => array(
 			'src' => 'http://gravatar.com/avatar/5b9c2b225b5c4ff91ffe849209153ecc?d=404&s=80&r=g', 
