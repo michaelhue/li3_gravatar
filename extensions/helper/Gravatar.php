e6b054bf71aa7713e81bace12797e08fba5702a9
Mehdi Lahmam B <mehdi@lahmam.com>
2012-12-30 14:21:27 +0100

    Make the default image parameter optional to let Gravatar serve their logo as a default image

--- extensions/helper/Gravatar.php
+++ extensions/helper/Gravatar.php
@@ -23,15 +23,15 @@ class Gravatar extends \lithium\template\Helper {
 	protected $_strings = array(
 		'gravatar_image' => '<img src="{:url}"{:options} />'
 	);
-	
+
 	/**
 	 * Generates an URL to the Gravatar for `'$email'`.
 	 *
 	 * @param string $email
 	 * @param array $options Optional options. Valid options are:
-	 *     - `'default'`: The fallback image that will be provided if there is no Gravatar associated 
-	 *     with `'$email'`. This may be a full URL, an absolute path, `'mm'`, `'identicon'`, 
-	 *     `'monsterid'`, `'wavatar'` or `'404'`. Defaults to `'404'`.
+	 *     - `'default'`: Optional fallback image that will be provided if there is no Gravatar associated
+	 *     with `'$email'`. This may be a full URL, an absolute path, `'mm'`, `'identicon'`,
+	 *     `'monsterid'`, `'wavatar'` or `'404'`.
 	 *     - `'size'`: Size of the image in pixels. Defaults to `'80'`.
 	 *     - `'rating'`: Required minimum rating of the Gravatar. This may be `'g'`, `'pg'`, `'r'` or
 	 *     `'x'`. If the Gravatar does not meet the rating the `'default'` option will be returned.
@@ -39,42 +39,43 @@ class Gravatar extends \lithium\template\Helper {
 	 * @return string Returns an URL to the Gravatar image.
 	 */
 	public function url($email, array $options = array()) {
-		$defaults = array('default' => 404, 'size' => 80, 'rating' => 'g');
+		$defaults = array('default' => null, 'size' => 80, 'rating' => 'g');
 		$options += $defaults;
-		
+
 		$request = $this->_context->request();
-		$defaultValues = array(404, 'mm', 'identicon', 'monsterid', 'wavatar');
+		$defaultValues = array(null, 404, 'mm', 'identicon', 'monsterid', 'wavatar');
 		$hash = md5(strtolower(trim($email)));
-	
+
 		if (!in_array($options['default'], $defaultValues) && strpos($options['default'], '://') === false) {
 			$scheme = $request->env('HTTPS') ? 'https://' : 'http://';
 			$host = $request->env('HTTP_HOST');
 			$options['default'] = $scheme . $host . $options['default'];
 		}
-		
+
 		if ($request->env('HTTPS')) {
-			$url = 'https://secure.gravatar.com';
+			$base = 'https://secure.gravatar.com';
 		} else {
-			$url = 'http://gravatar.com';
+			$base = 'http://gravatar.com';
 		}
-		$url .= "/avatar/{$hash}";
-		$url .= "?d=" . urlencode($options['default']);
-		$url .= "&s={$options['size']}";
-		$url .= "&r={$options['rating']}";
-		
-		return $url;
+		$query = array(
+			'd' => $options['default'],
+			's' => $options['size'],
+			'r' => $options['rating']
+		);
+
+		return "{$base}/avatar/{$hash}" . '?' . http_build_query($query);
 	}
 
 	/**
 	 * Renders the Gravatar for `'$email'`.
 	 *
 	 * @see li3_gravatar\extensions\helper\Gravatar::url()
-	 * @param string $email  
+	 * @param string $email
 	 * @param array $options Optional options.
 	 * @return string Returns an image tag displaying the Gravatar.
 	 */
 	public function image($email, array $options = array()) {
-		$defaults = array('default' => 404, 'size' => 80, 'rating' => 'g', 'alt' => '');
+		$defaults = array('default' => null, 'size' => 80, 'rating' => 'g', 'alt' => '');
 		$options += $defaults;
 		$url = $this->url($email, $options);
 		unset($options['default'], $options['size'], $options['rating']);
